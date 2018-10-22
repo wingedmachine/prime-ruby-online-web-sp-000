9f32c193c2134b19687e606a786485d6c7eaec9e
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 23:23:56 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1
+
+  divisor = 2
+  upper_limit = num
+  while divisor < upper_limit
+    return false if num % divisor == 0
+
+    upper_limit = num / divisor
+    divisor += 1
+  end
+  true
+end

016555e6800201d22b2749aaa6568866567279af
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-22 23:22:52 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(num)
+  divisor = 2
+  upper_limit = num
+  while divisor < upper_limit
+    return false if num % divisor == 0
+
+    upper_limit = num / divisor
+    divisor += 1
+  end
+end
